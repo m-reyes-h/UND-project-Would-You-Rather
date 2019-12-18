9efeb42aa0dd4102b71c1c6452fc2f8e727bcba5
Michel Reyes <mreyesh85@gmail.com>
2019-12-18 08:36:10 -0500

    firts-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Navbar, Nav, Container} from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar expand="lg" fixed="top" variant="light" className="main-navbar">
+      <Container>
+        <Navbar.Brand href="#home">Title</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">New question</Nav.Link>
+            <Nav.Link href="#link">Leader board</Nav.Link>
+          </Nav>
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">User</Nav.Link>
+            <Nav.Link href="#link">Logout</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
