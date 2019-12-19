8acc4bc46997ecd2a43664dea639f054632f4007
Michel Reyes <mreyesh85@gmail.com>
2019-12-19 09:04:35 -0500

    ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,127 @@
-import React from 'react';
-import {Navbar, Nav, Container} from 'react-bootstrap'
+import React, { Fragment, Component } from "react";
+import { Navbar, Nav, Container, Row, Col } from "react-bootstrap";
+import ScrollMenu from "react-horizontal-scrolling-menu";
 
-function App() {
+const list = [
+  { name: "item1" },
+  { name: "item2" },
+  { name: "item3" },
+  { name: "item4" },
+  { name: "item5" },
+  { name: "item6" },
+  { name: "item7" },
+  { name: "item8" },
+  { name: "item9" },
+  { name: "item10" }
+];
+
+// One item component
+// selected prop will be passed
+const MenuItem = ({ text }) => {
   return (
-    <Navbar expand="lg" fixed="top" variant="light" className="main-navbar">
-      <Container>
-        <Navbar.Brand href="#home">Title</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">New question</Nav.Link>
-            <Nav.Link href="#link">Leader board</Nav.Link>
-          </Nav>
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">User</Nav.Link>
-            <Nav.Link href="#link">Logout</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div className="menu-item">
+      <div className="menu-item-card d-flex flex-column justify-center  text-left card-user1"></div>
+      <div className="text-wrapper">
+        <span className="d-inline-block menu-item-title">Sara Edo asks</span>
+        <p>Would you like be a front end or ...</p>
+        <button className="button">View Pull</button>
+      </div>
+    </div>
   );
+};
+
+const Arrow = ({ text, className }) => {
+  return <div className={className}>{text}</div>;
+};
+
+const ArrowLeft = Arrow({ text: "", className: "arrow-prev" });
+const ArrowRight = Arrow({ text: "", className: "arrow-next" });
+
+// All items component
+// Important! add unique key
+export const Menu = (list, selected) =>
+  list.map(el => {
+    const { name } = el;
+
+    return <MenuItem text={name} key={name} />;
+  });
+
+class App extends Component {
+  state = {
+    selected: list[0]
+  };
+
+  menu = Menu(list, this.state.selected);
+
+  render() {
+    return (
+      <Fragment>
+        <Navbar expand="lg" fixed="top" variant="light" className="main-navbar">
+          <Container>
+            <Navbar.Brand href="#home">Title</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">New question</Nav.Link>
+                <Nav.Link href="#link">Leader board</Nav.Link>
+              </Nav>
+              <Nav className="ml-auto">
+                <Nav.Link href="#home">User</Nav.Link>
+                <Nav.Link href="#link">Logout</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
+        <Container role="main">
+          <Row>
+            <Col>
+              <div className="answer-section">
+                <h2>Unanswered Questions</h2>
+                <div className="scroll-menu-wrapper">
+                  {
+                    <ScrollMenu
+                      alignCenter={false}
+                      hideSingleArrow={true}
+                      data={this.menu}
+                      arrowLeft={ArrowLeft}
+                      arrowRight={ArrowRight}
+                      selected={this.state.selected}
+                      onSelect={this.onSelect}
+                      wheel={false}
+                    />
+                  }
+                </div>
+              </div>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col>
+              <div className="answer-section">
+                <h2>Answered Questions</h2>
+                <div className="scroll-menu-wrapper">
+                  {
+                    <ScrollMenu
+                      alignCenter={false}
+                      hideSingleArrow={true}
+                      data={this.menu}
+                      arrowLeft={ArrowLeft}
+                      arrowRight={ArrowRight}
+                      selected={this.state.selected}
+                      onSelect={this.onSelect}
+                      wheel={false}
+                    />
+                  }
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
