e5dc6459bc178032fbb159944d2c8ccb3e478f15
Michel Reyes <mreyesh85@gmail.com>
2020-01-04 21:16:13 -0500

    Updates

---
+++ precache-manifest.b6884f218ae95e63763551b902cfb52f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d67dde9b11561596991fb1a31609f3ba",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/index.html"
+  },
+  {
+    "revision": "3869619c79686cf20b29",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "db36fd237a48de738bcb",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/css/main.0a0a1905.chunk.css"
+  },
+  {
+    "revision": "3869619c79686cf20b29",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/js/2.115969b0.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/js/2.115969b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db36fd237a48de738bcb",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/js/main.1ddb46dc.chunk.js"
+  },
+  {
+    "revision": "89eb1ab7257806264df5",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/js/runtime-main.76161d0a.js"
+  },
+  {
+    "revision": "301a936fa13134d6b06f3929ba7c0356",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/media/check_circle-24px.301a936f.svg"
+  },
+  {
+    "revision": "e31eade43b62735a559493dbbbd7bdf9",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/media/check_circle_outline-24px.e31eade4.svg"
+  },
+  {
+    "revision": "f8e08e9215847c5e2f20d584414f42af",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/media/circle_outline-24px.f8e08e92.svg"
+  },
+  {
+    "revision": "85d4c7f6dea4c012e9ec539b01a3082c",
+    "url": "/m-reyes-h/UND-project-Would-You-Rather/static/media/question_answer-24px.85d4c7f6.svg"
+  }
+]);
